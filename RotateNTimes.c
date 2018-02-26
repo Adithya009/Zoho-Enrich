335d67dd9250eedbaba7f3d004f888d31267f867
Sangeetha Sundar <thestrongbattles@gmail.com>
2018-02-26 16:09:55 +0530

    Create RotateNTimes.c

--- RotateNTimes.c
+++ RotateNTimes.c
@@ -19,6 +19,9 @@ int main()
    char a[100];
    int x,i=0;
    scanf("%s%d",a,&x);
+   /* If the number is reversed n times, it would start with the nth digit till the end
+      And then from index 0 till n-1
+   */
    printf("The rotated number is");
    for(i=x;i<strlen(a);i++)
    {
